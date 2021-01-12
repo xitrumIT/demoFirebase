3f18ed78bcb4aafa470ba1a6e3184cdfa4a39d95
xitrumIT <toiday_456@yahoo.com>
2021-01-12 16:26:13 +0700

    fix: change eslin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,60 @@
 module.exports = {
-   env: {
-      browser: true,
-      es6: true,
-      node: true,
-      "react-native/react-native": true,
-   },
-   extends: ["eslint:recommended", "prettier"],
-   globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly",
-   },
-   parser: "babel-eslint",
-   parserOptions: {
-      ecmaFeatures: {
-         tsx: true,
-         jsx: true,
-         js: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: "module",
-   },
-   plugins: ["react", "react-native"],
-   rules: {
-      "react-native/no-unused-styles": 2,
-      "react-native/split-platform-components": 2,
-      "react-native/no-inline-styles": 2,
-      // khai bao mau
-      "react-native/no-color-literals": 2,
-      "react-native/no-raw-text": 2,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  extends: ['eslint:recommended', '@react-native-community'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true,
+      jsx: true,
+      js: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    // Khong su dung style se bao do
+    'react-native/no-unused-styles': 2,
+    // Tu dong sap xep style
+    'react-native/sort-styles': ['error', 'desc'],
+    // Kiem tra code xem co style inline hay ko
+    'react-native/no-inline-styles': 2,
+    // // khai bao mau
+    // "react-native/no-color-literals": 2,
+    // Thut le
+    indent: ['error', 2],
 
-      "react/no-children-prop": "error",
-      "react/no-deprecated": "error",
-      "react/no-direct-mutation-state": "error",
-      "react/no-typos": "error",
-      "react/no-string-refs": "error",
-      "react/no-this-in-sfc": "error",
-      "react/no-unsafe": "error",
-      "react/no-unused-state": "warn",
-      // "react/prop-types": "none",
-      "react/react-in-jsx-scope": "error",
-      "prettier/prettier": [
-         "error",
-         {
-            printWidth: 80,
-            trailingComma: "es5",
-            semi: false,
-            jsxSingleQuote: true,
-            singleQuote: true,
-            useTabs: true,
-         },
-      ],
-      "react-native/sort-styles": [
-         "error",
-         "asc",
-         {
-            ignoreClassNames: false,
-            ignoreStyleProperties: false,
-         },
-      ],
-      indent: ["error", 3],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "double"],
-      semi: ["error", "always"],
-   },
+    // "prettier/prettier": [
+    //    "error",
+    //    {
+    //       printWidth: 80,
+    //       trailingComma: "es5",
+    //       semi: false,
+    //       jsxSingleQuote: true,
+    //       singleQuote: true,
+    //       useTabs: true,
+    //    },
+    // ],
+    // "react-native/sort-styles": [
+    //    "error",
+    //    "asc",
+    //    {
+    //       ignoreClassNames: false,
+    //       ignoreStyleProperties: false,
+    //    },
+    // ],
+
+    //  "linebreak-style": ["error", "unix"],
+    // quotes: ["error", "double"],
+    // semi: ["error", "always"],
+  },
 };
